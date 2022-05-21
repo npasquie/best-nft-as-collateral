010d37689c9e8163178956a11d99199ab3af13cf
leo <leonard.ll.l81@gmail.com>
2022-05-21 23:04:22 +0200

    🐛  refresh error fix attempt

--- nft-as-collateral-ui/next.config.js
+++ nft-as-collateral-ui/next.config.js
@@ -24,7 +24,7 @@ const nextConfig = withImages({
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    // domains: ['wild-dream-9169.on.fleek.co', 'localhost:3000'],
+    domains: ['wild-dream-9169.on.fleek.co', 'localhost:3000'],
   },
   module: {
     rules: [
